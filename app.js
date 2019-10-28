cd5c1c543a754a852ef845d26e406433fae672ab
Nick Sherman <ogleffeckt2@yahoo.com>
2019-10-28 18:50:18 -0400

    implementing search

--- app.js
+++ app.js
@@ -1,36 +1,35 @@
 require('dotenv').config();
 
-const bodyParser     = require('body-parser');
-const cookieParser   = require('cookie-parser');
-const express        = require('express');
-const favicon        = require('serve-favicon');
-const hbs            = require('hbs');
-const mongoose       = require('mongoose');
-const logger         = require('morgan');
-const path           = require('path');
-const session        = require("express-session");
-const bcrypt         = require('bcryptjs');
-const MongoStore     = require("connect-mongo")(session);
-const flash          = require("express-flash")
-const passport       = require("passport");
-const LocalStrategy  = require("passport-local").Strategy;
-const User           = require('./models/User');
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const SlackStrategy  = require("passport-slack").Strategy;
-const axios          = require("axios");
-const multer         = require("multer");
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const express = require('express');
+const favicon = require('serve-favicon');
+const hbs = require('hbs');
+const mongoose = require('mongoose');
+const logger = require('morgan');
+const path = require('path');
+const session = require('express-session');
+const bcrypt = require('bcryptjs');
+const MongoStore = require('connect-mongo')(session);
+const flash = require('express-flash');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const User = require('./models/User');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const SlackStrategy = require('passport-slack').Strategy;
+const axios = require('axios');
+const multer = require('multer');
 // const { userAuth } = require('../middleware/auth')
-
-
 
 mongoose.Promise = Promise;
 mongoose
-  .connect(process.env.MONGODB_URI, {useMongoClient: true})
-  .then(() => {
-    console.log('Connected to Mongo!')
-  }).catch(err => {
-    console.error('Error connecting to mongo', err)
-  });
+	.connect(process.env.MONGODB_URI, { useMongoClient: true })
+	.then(() => {
+		console.log('Connected to Mongo!');
+	})
+	.catch((err) => {
+		console.error('Error connecting to mongo', err);
+	});
 
 const app_name = require('./package.json').name;
 const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
@@ -42,151 +41,177 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(session({
-  secret: "our-passport-local-strategy-app",
-  resave: true,
-  saveUninitialized: true
-}));
+app.use(
+	session({
+		secret: 'our-passport-local-strategy-app',
+		resave: true,
+		saveUninitialized: true
+	})
+);
 
 passport.serializeUser((user, cb) => {
-  cb(null, user._id);
+	cb(null, user._id);
 });
 
 passport.deserializeUser((id, cb) => {
-  User.findById(id, (err, user) => {
-    if (err) { return cb(err); }
-    cb(null, user);
-  });
+	User.findById(id, (err, user) => {
+		if (err) {
+			return cb(err);
+		}
+		cb(null, user);
+	});
 });
 
 app.use(flash());
-passport.use(new LocalStrategy((username, password, next) => {
-  User.findOne({ username }, (err, user) => {
-    if (err) {
-      return next(err);
-    }
-    if (!user) {
-      return next(null, false, { message: "Incorrect username" });
-    }
-    if (!bcrypt.compareSync(password, user.password)) {
-      return next(null, false, { message: "Incorrect password" });
-    }
+passport.use(
+	new LocalStrategy((username, password, next) => {
+		User.findOne({ username }, (err, user) => {
+			if (err) {
+				return next(err);
+			}
+			if (!user) {
+				return next(null, false, { message: 'Incorrect username' });
+			}
+			if (!bcrypt.compareSync(password, user.password)) {
+				return next(null, false, { message: 'Incorrect password' });
+			}
 
-    return next(null, user);
-  });
-}));
+			return next(null, user);
+		});
+	})
+);
 app.use(passport.initialize());
 app.use(passport.session());
 
 // Express View engine setup
 
-app.use(require('node-sass-middleware')({
-  src:  path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  sourceMap: true
-}));
-      
+app.use(
+	require('node-sass-middleware')({
+		src: path.join(__dirname, 'public'),
+		dest: path.join(__dirname, 'public'),
+		sourceMap: true
+	})
+);
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
-console.log(path.join(__dirname, 'public'))
+console.log(path.join(__dirname, 'public'));
 
 // default value for title local
 app.locals.title = 'Hobby Helper';
 
-app.use(session({
-  secret: "shhh-super-sectet-key",
-  cookie: { maxAge: 60000 },
-  store: new MongoStore({
-    mongooseConnection: mongoose.connection,
-    ttl: 24 * 60 * 60 // 1 day
-  })
-}));
+app.use(
+	session({
+		secret: 'shhh-super-sectet-key',
+		cookie: { maxAge: 60000 },
+		store: new MongoStore({
+			mongooseConnection: mongoose.connection,
+			ttl: 24 * 60 * 60 // 1 day
+		})
+	})
+);
 
 app.use((req, res, next) => {
-  if (req.user) req.user.isAdmin = req.user.role === 'Admin'
-  res.locals.currentUser = req.user; 
-  next();
- });
+	if (req.user) req.user.isAdmin = req.user.role === 'Admin';
+	res.locals.currentUser = req.user;
+	next();
+});
 
 passport.use(
-  new GoogleStrategy(
-    {
-      clientID: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/google/callback"
-    },
-    (accessToken, refreshToken, profile, done) => {
-      console.log("Google account details:", profile);
+	new GoogleStrategy(
+		{
+			clientID: process.env.GOOGLE_CLIENT_ID,
+			clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+			callbackURL: '/google/callback'
+		},
+		(accessToken, refreshToken, profile, done) => {
+			console.log('Google account details:', profile);
 
-      User.findOne({ googleID: profile.id })
-        .then(user => {
-          if (user) {
-            done(null, user);
-            return;
-          }
+			User.findOne({ googleID: profile.id })
+				.then((user) => {
+					if (user) {
+						done(null, user);
+						return;
+					}
 
-          let theImage = "";
+					let theImage = '';
 
-          if(profile.photos){
-            theImage = profile.photos[0].value;
-          }
+					if (profile.photos) {
+						theImage = profile.photos[0].value;
+					}
 
-          User.create({
-             googleID: profile.id,
-             isAdmin: false,
-             googlePic: theImage,
-             username: profile._json.name
-
-            })
-            .then(newUser => {
-              done(null, newUser);
-            })
-            .catch(err => done(err)); 
-        })
-        .catch(err => done(err)); 
-    }
-  )
+					User.create({
+						googleID: profile.id,
+						isAdmin: false,
+						googlePic: theImage,
+						username: profile._json.name
+					})
+						.then((newUser) => {
+							done(null, newUser);
+						})
+						.catch((err) => done(err));
+				})
+				.catch((err) => done(err));
+		}
+	)
 );
 
 passport.use(
-  new SlackStrategy(
-    {
-      clientID: process.env.SLACK_CLIENT_ID,
-      clientSecret: process.env.SLACK_CLIENT_SECRET,
-      callbackURL: "/auth/slack/callback"
-    },
-    (accessToken, refreshToken, profile, done) => {
-      // to see the structure of the data in received response:
-      console.log("Slack account details:", profile);
+	new SlackStrategy(
+		{
+			clientID: process.env.SLACK_CLIENT_ID,
+			clientSecret: process.env.SLACK_CLIENT_SECRET,
+			callbackURL: '/auth/slack/callback'
+		},
+		(accessToken, refreshToken, profile, done) => {
+			// to see the structure of the data in received response:
+			console.log('Slack account details:', profile);
 
-      User.findOne({ slackID: profile.id })
-        .then(user => {
-          if (user) {
-            done(null, user);
-            return;
-          }
+			User.findOne({ slackID: profile.id })
+				.then((user) => {
+					if (user) {
+						done(null, user);
+						return;
+					}
 
-          User.create({
-           slackID: profile.id,
-           displayName: profile.displayName,
-           slackPic: profile.user.image_512
-          })
-            .then(newUser => {
-              console.log(newUser)
-              done(null, newUser);
-            })
-            .catch(err => done(err)); // closes User.create()
-        })
-        .catch(err => done(err)); // closes User.findOne()
-    }
-  )
+					User.create({
+						slackID: profile.id,
+						displayName: profile.displayName,
+						slackPic: profile.user.image_512
+					})
+						.then((newUser) => {
+							console.log(newUser);
+							done(null, newUser);
+						})
+						.catch((err) => done(err)); // closes User.create()
+				})
+				.catch((err) => done(err)); // closes User.findOne()
+		}
+	)
 );
+
+function search() {
+	var name = document.getElementById('searchForm').elements['searchItem'].value;
+	var pattern = name.toLowerCase();
+	var targetId = '';
+
+	var divs = document.getElementsByClassName('col-sm-3');
+	for (var i = 0; i < divs.length; i++) {
+		var para = divs[i].getElementsByTagName('a');
+		var index = para[0].innerText.toLowerCase().indexOf(pattern);
+		if (index != -1) {
+			targetId = divs[i].parentNode.id;
+			document.getElementById(targetId).scrollIntoView();
+			break;
+		}
+	}
+}
 
 const user = require('./routes/user-routes');
-app.use('/', user)
+app.use('/', user);
 
 const index = require('./routes/index');
 app.use('/', index);
--- routes/auth.js
+++ routes/auth.js
@@ -2,77 +2,53 @@ const express = require('express');
 const bcrypt = require('bcryptjs');
 const User = require('../models/User');
 const router = express.Router();
-const passport = require('passport')
-const { userAuth } = require('../middleware/auth')
-
-// router.post('/signup', (req, res, next) => {
-//     const salt = bcrypt.genSaltSync(10)
-//     const userData = {
-//         username: req.body.username,
-//         password: bcrypt.hashSync(req.body.password, salt),
-//         role: (req.user && req.user.role === "Admin") ? req.body.role : "Admin"
-//     }
-
-//     User.create(userData)
-//         .then(user => {
-//             req.session.currentUser = user
-//             req.logIn(user, (err) => {
-//                 if (!err) {
-//                     res.status(201).redirect('/')
-//                     req.flash('success', 'Registered and logged in!');
-//                 } else next(err)
-//             })
-//         })
-//         .catch(e => res.status(500).send(e))
-// });
+const passport = require('passport');
+const { userAuth } = require('../middleware/auth');
 
 router.get('/login', (req, res, next) => {
-    res.render('users/login')
-})
+	res.render('users/login');
+});
 
 router.post('/login', function(req, res, next) {
-    passport.authenticate('local', function(err, user, info) {
-        if (err) {
-            req.flash('failure', `${info.message}`);
-            return next(err);
-        }
-        if (!user) {
-            req.flash('failure', `${info.message}`);
-            return res.redirect('/login');
-        }
-        req.logIn(user, function(err) {
-            if (err) {
-                req.flash('failure', `${info.message}`);
-                return next(err);
-            }
-            req.flash('success', 'Successfully logged in!');
-            return res.redirect('/');
-        });
-    })(req, res, next);
+	passport.authenticate('local', function(err, user, info) {
+		if (err) {
+			req.flash('failure', `${info.message}`);
+			return next(err);
+		}
+		if (!user) {
+			req.flash('failure', `${info.message}`);
+			return res.redirect('/login');
+		}
+		req.logIn(user, function(err) {
+			if (err) {
+				req.flash('failure', `${info.message}`);
+				return next(err);
+			}
+			req.flash('success', 'Successfully logged in!');
+			return res.redirect('/');
+		});
+	})(req, res, next);
 });
 
 router.post('/logout', userAuth, (req, res, next) => {
-    req.flash('success', 'Logged out!');
-    req.logout();
-    res.redirect('/');
-})
+	req.flash('success', 'Logged out!');
+	req.logout();
+	res.redirect('/');
+});
 
 router.get(
-    "/auth/google",
-    passport.authenticate("google", {
-        scope: [
-            "https://www.googleapis.com/auth/userinfo.profile",
-            "https://www.googleapis.com/auth/userinfo.email"
-        ]
-    })
+	'/auth/google',
+	passport.authenticate('google', {
+		scope: [ 'https://www.googleapis.com/auth/userinfo.profile', 'https://www.googleapis.com/auth/userinfo.email' ]
+	})
 );
 
 router.get(
-    "/google/callback",
-    passport.authenticate("google", {
-        successRedirect: "/",
-        failureRedirect: "/login"
-    })
+	'/google/callback',
+	passport.authenticate('google', {
+		successRedirect: '/',
+		failureRedirect: '/login'
+	})
 );
 
-module.exports = router
+module.exports = router;
